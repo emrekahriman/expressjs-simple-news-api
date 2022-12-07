b9c37ce13fb7e678c50951ae669122e662946324
Emre Kahriman <emrekahriman041@gmail.com>
2022-12-07 20:04:50 +0300

    Route created for check token

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -69,7 +69,36 @@ const postLogin = async (req, res) => {
 };
 
 
+const checkToken = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id });
+
+        if (!user) {
+            res.json({
+                status: 404,
+                message: "User not found.",
+            });
+        } else {
+            res.json({
+                status: 200,
+                message: "Token is valid.",
+                user: decoded,
+            });
+        }
+
+    } catch (err) {
+        res.json({
+            status: 500,
+            message: err.message,
+        });
+    }
+};
+
+
 module.exports = {
     postRegister,
     postLogin,
+    checkToken,
 };
--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { postRegister, postLogin } = require('../controllers/AccountController');
+const { postRegister, postLogin, checkToken } = require('../controllers/AccountController');
 
 
 router.post('/register', postRegister);
 router.post('/login', postLogin);
+router.get('/check-token', checkToken);
 
 
 module.exports = router;
